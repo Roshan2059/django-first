d62c0f623bb083ab7ac49e7d1b11d52490cf6090
Roshan2059 <roshanpanta2059@gmail.com>
2022-09-15 18:00:49 +0545

    finished todays module

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('about', about, name='about'),
+    path('contact', contact, name='contact'),
+    path('portfolio', portfolio, name='portfolio'),
+    path('price', price, name='price'),
+    path('services', services, name='services'),
 ]
