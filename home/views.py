6b45ff2e6daab388029e9e9d126cc023afcc75d9
Roshan2059 <roshanpanta2059@gmail.com>
2022-09-18 17:31:55 +0545

    contact.html bata submit gariyo

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def home(request):
@@ -9,6 +9,13 @@ def about(request):
     return render(request, 'about.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        data = Contact.objects.create(name = name,email = email,subject = subject,message = message)
+        data.save()
     return render(request, 'contact.html')
 
 def portfolio(request):
